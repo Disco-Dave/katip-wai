4ead9e798ab7a0ec842fa5a570c51d075a71f83c
disco-dave <1124825-Disco-Dave@users.noreply.gitlab.com>
2022-11-12 18:40:45 -0500

    added publish.sh script and updated ci

---
+++ scripts/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+ghc_version="ghc-8.10.7"
+
+repo_path="$(realpath "$0" | xargs dirname | xargs dirname)"
+cd "$repo_path"
+
+dir=$(mktemp -d dist-docs.XXXXXX)
+trap 'rm -r "$dir"' EXIT
+
+cabal sdist --builddir="$dir"
+cabal haddock --with-compiler="$ghc_version" --builddir="$dir" --haddock-for-hackage --enable-doc
+
+cabal upload "$@" "$dir"/sdist/*.tar.gz
+cabal upload --documentation "$@" "$dir"/*-docs.tar.gz
